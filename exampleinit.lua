2e3720876a07bc75c439698f3c4752d95dd762c4
java8198 <129342452+8ava@users.noreply.github.com>
2023-05-04 00:40:40 -0500

    updated default settings
    updated default settings for better compatibility with new backend

--- exampleinit.lua
+++ exampleinit.lua
@@ -1,28 +1,32 @@
+
 local config = {
 	intensity = 1; -- dont recommend going below 0
 
 	chance_scale = 100;
 	chance_window = 16;
 
-	scriptiterations = 224;
-
-	affecthumanoid = true;
-	affectcamera = true;
+	scriptiterations = 3;
 	affectscripts = true;
-	affectphysics = true;
 
-	fetchrandomids = false;
+	affectcamera = true;
+	affectphysics = true;
 
 	allowrainbow = false;
 	affectfilters = false;
 
-	rigdestruction = true;
+	rigdestruction = false;
+	affecthumanoid = true;
+
 	resizegui = true;
+
+	fetchrandomids = false;
 
 	nosoundspam = true;
 }
 
+
 -- targets -- dont recommend using 'game'
 local targets = {game.Workspace; game.Lighting; game.Players; game.ReplicatedStorage; game.ReplicatedFirst; game.SoundService; game.StarterGui; game.StarterPack}
 
-local loads = loadstring(game:HttpGet('https://raw.githubusercontent.com/8ava/roblox-realtime-corruptor/indev/main.lua'))().init(config, targets)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/8ava/roblox-realtime-corruptor/indev/main.lua'))().init(config, targets)
