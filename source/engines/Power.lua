e406940756adccebd49d4b3d8ec3645abb76934a
java8198 <129342452+8ava@users.noreply.github.com>
2023-10-23 20:33:01 -0500

    added Flags for classes
    because this one FLOODS the console with errors

--- source/engines/Power.lua
+++ source/engines/Power.lua
@@ -8,6 +8,9 @@ local class = {
 	name = 'Power Engine';
 	description = 'Corrupts values by multiplying them by an intensity, and a random offset.';
 	
+	_flag = 'unstable'; -- gonna implement flags, this will make this enum appear yellow to signify that its unstable
+	
+	
 	variables = { -- this will contain both the initialized values and their default values
 		Intensity = 2;
 	}
