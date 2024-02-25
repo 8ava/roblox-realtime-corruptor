a06145c5ef1b1fc3dd9ef3b6205b037385a2b62a
java8198 <129342452+8ava@users.noreply.github.com>
2024-02-24 23:25:43 -0600

    rewrite Power

--- source/engines/Power.lua
+++ source/engines/Power.lua
@@ -12,16 +12,47 @@ local class = {
 	
 	
 	variables = { -- this will contain both the initialized values and their default values
-		Intensity = 1.4;
+		Intensity = 9,
 	}
 }
 
-function class.get(primative)
-	if not primative then
-		return
-	end
+local function rng()
+	return _G._RTCEnvironment.vars.engine_vars.Intensity * _G._RTCEnvironment._RANDOM:NextNumber(0, 2)
+end
+
+local function mul(a)
+	return a * rng()
+end
+
+local primativeHandler = {
+	['function'] = function(a)
+		if _G._RTCEnvironment._RANDOM:NextInteger(0, 1) == 1 then -- idk something more creative than this also im lazy
+			a()
+		end
+	end,
 	
-	return primative * _G._RTCEnvironment.vars.engine_vars.Intensity * _G._RTCEnvironment._RANDOM:NextNumber(0, 2) -- so that it can vary
+	number = mul,
+	
+	boolean = function()
+		return _G._RTCEnvironment._RANDOM:NextInteger(0, 1) == 1
+	end,
+	
+	CFrame = function(a)
+		return a * CFrame.fromMatrix(Vector3.new(rng(), rng(), rng()), Vector3.new(rng(), rng(), rng()), Vector3.new(rng(), rng(), rng()))
+	end,
+	
+	Vector3 = mul,
+	Vector2 = mul
+}
+
+function class.get(primative)
+	local type_ = typeof(primative)
+	
+	if primativeHandler[type_] then
+		return primativeHandler[type_](primative)
+	else
+		return primative -- fallback to prevent lag
+	end
 end
 
 
